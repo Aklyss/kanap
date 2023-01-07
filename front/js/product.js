1a5d1cdc62f55efe413daeb931efc8a0c4c28ca1
Aklyss <romuald-samson@hotmail.com>
2023-01-07 14:32:13 +0100

    product

--- front/js/product.js
+++ front/js/product.js
@@ -1,20 +1,22 @@
 const item__img = document.querySelector('.item__img') 
-console.log(item__img);
+
 const title = document.getElementById('title')
-console.log(title);
+
 const price = document.getElementById('price')
-console.log(price);
+
 const description = document.getElementById('description')
-console.log(description);
+
+const colors = document.getElementById('colors')
 
 
 const url_id = window.location.search;
-console.log(url_id);
+
 
 const Id = url_id.slice(4);
-console.log(Id);
+
 
 const url_url = (`http://localhost:3000/api/products/${Id}`)
+
 
     fetch(url_url)
     .then(reponse => reponse.json())
@@ -23,6 +25,9 @@ const url_url = (`http://localhost:3000/api/products/${Id}`)
         price.innerHTML += data.price
         description.innerHTML += data.description
         item__img.innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}"></img>`
+        data.colors.forEach((elementColor) => {
+            colors.innerHTML += `<option value="${elementColor}">${elementColor}</option>`
+        });
     }
     )
 
